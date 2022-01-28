544895fd85200ee5ac81ca1c173a7a03546f0312
Alexander Saenko <saenko.pirateit@gmail.com>
2022-01-28 12:16:26 +0300

    docs: Update README

--- 04-streams/index.js
+++ 04-streams/index.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const Buffer = require('buffer').Buffer;
+const Transform = require('stream').Transform;
+const { once } = require('events');
 const readline = require('readline');
 const randomNumber = require('./utils/randomNumber');
 const mergeSort = require('./utils/mergeSort');
-var Transform = require('stream').Transform;
 
 const createFile = () => {
   const fileSize = 10; // File size in MB
@@ -28,7 +29,6 @@ const createFile = () => {
   process.stdout.write('File with random numbers created');
 };
 
-// createFile();
 
 let filesCounter = 1;
 const splitFile = () => {
@@ -38,84 +38,85 @@ const splitFile = () => {
     }
 
     const readableStream = fs.createReadStream('./_numbers.txt');
-    // const rl = readline.createInterface({
-    //   input: readableStream,
-    //   crlfDelay: Infinity
-    // });
+    let filesCounter = 0;
+    let linesCounter = 0;
 
-    // let resultBuffer = Buffer.alloc(0);
-    // let writableStream;
-    let filesCounter = 1;
-    // let arr = [];
-    // rl.on('line', (input) => {
-    //   if (Buffer.byteLength(resultBuffer) < 131072) { // 1 MB = 1048576
-    //     resultBuffer = Buffer.concat([resultBuffer, Buffer.from(input + '\n')]);
-    //   } else {
-    //     writableStream = fs.createWriteStream(`./temp/${filesCounter}.txt`);
-    //     resultBuffer.toString().split('\n').forEach(num => arr.push(num));
-    //     arr = mergeSort(arr);
-    //     writableStream.write(resultBuffer.toString());
-    //     writableStream.end();
-    //     resultBuffer = Buffer.alloc(0);
-    //     resultBuffer = Buffer.concat([resultBuffer, Buffer.from(input + '\n')]);
-    //     filesCounter += 1;
-    //   }
-    // })
-    //   .on('close', () => {
-    //     writableStream = fs.createWriteStream(`./temp/${filesCounter}.txt`);
-    //     writableStream.write(resultBuffer.toString());
-    //     writableStream.end();
-    //     resultBuffer = Buffer.alloc(0);
-    //     filesCounter += 1;
-    //     resolve();
-    //   });
-    var liner = new Transform( { objectMode: true } );
+    const liner = new Transform({ objectMode: true });
     liner._transform = function (chunk, encoding, done) {
-      var data = chunk.toString();
+      let data = chunk.toString();
       if (this._lastLineData) data = this._lastLineData + data;
 
-      var lines = data.split('\n');
-      this._lastLineData = lines.splice(lines.length-1,1)[0];
-      const arr = []
-      // lines.forEach(arr.push(this));
-      const writableStream = fs.createWriteStream(`./temp/${filesCounter}.txt`);
-      writableStream.write(lines.join('\n'))
+      let lines = data.split('\n');
+      this._lastLineData = lines.splice(lines.length - 1, 1)[0];
+      lines = mergeSort(lines);
+      linesCounter += lines.length;
       filesCounter++
-      console.log(lines)
+      const writableStream = fs.createWriteStream(`./temp/${filesCounter}.txt`);
+      writableStream.write(lines.join('\n') + '\n');
       done();
- }
+    }
 
- // to flush remaining data (if any)
- liner._flush = function (done) {
+    liner._flush = function (done) {
       if (this._lastLineData) this.push(this._lastLineData);
       this._lastLineData = null;
       done();
- }
-    readableStream.pipe(liner)
+    }
 
+    readableStream.on('end', () => resolve({ filesCounter, linesCounter }));
+    readableStream.pipe(liner);
   })
 };
-// splitFile();
 
-const joinFiles = () => {
-  const writableStream = fs.createWriteStream('./sorted_numbers.txt');
-  const streams = []
-  const indexes = []
-  for (let i = 1; i <= filesCounter; i += 1) {
-    const stream = fs.createReadStream(`./temp/${i}.txt`)
-    streams.push(stream)
+const joinFiles = async (files) => {
+  console.log(files)
+
+  const streams = [];
+  for (let i = 1; i < files.filesCounter; i += 1) {
+    const readable = fs.createReadStream(`./temp/${i}.txt`);
+
+    // readable.on('end', () => {
+    //   if (streams[i + 1]) {
+    //     streams[i + 1].resume();
+    //   }
+    // });
+
+    streams.push(readable);
   }
 
-  // const rl = readline.createInterface({
-  //   input: streams,
-  //   crlfDelay: Infinity
-  // });
-  console.log(indexes)
-};
+    let min = -1;
+    // for (let i = 1; i < 5; i++) {
+    let count = 0;
+    let mid = 99999;
+    (async function google(stream = streams[0]) {
+      console.log(min)
+      try {
+        const rl = readline.createInterface({
+          input: stream.resume(),
+          crlfDelay: Infinity
+        });
 
-// joinFiles();
+        rl.on('line', (line) => {
+          if (Number(line) > min && Number(line) < mid) {
+            mid = Number(line)
+            min = mid;
+          }
+        });
+
+        await once(rl, 'close');
+
+        count += 1
+      } catch (err) {
+        console.error(err);
+      }
+      min = mid;
+      return google(streams[count])
+    })();
+  // }
+
+  // fs.appendFile('./sorted_numbers.txt', String(min), (err) => console.log(err))
+}
 
 (async () => {
-  await splitFile()
-  // joinFiles();
+  const files = await splitFile();
+  joinFiles(files);
 })()
--- README.md
+++ README.md
@@ -16,11 +16,29 @@
     <li>
       <a href="#01-lesson---introduction-to-node-and-npm">01 Lesson - Introduction to Node and NPM</a>
     </li>
+    <li>
+      <a href="#04-lesson---streams">01 Lesson - Streams</a>
+    </li>
+    <li>
+      <a href="#05-lesson---testing">05 Lesson - Testing</a>
+    </li>
   </ol>
 </details>
 
 ## 01 Lesson - Introduction to Node and NPM
 
 [Link to...](01-node)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## 04 Lesson - Streams
+
+[Link to...](04-streams)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+## 05 Lesson - Testing
+
+[Link to...](05-testing)
 
 <p align="right">(<a href="#top">back to top</a>)</p>
